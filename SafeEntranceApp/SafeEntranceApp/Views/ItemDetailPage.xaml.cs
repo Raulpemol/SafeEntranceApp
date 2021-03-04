dec24ae57327c315e4ea20db40edc8606a0f3713
Raulpemol <UO263743@uniovi.es>
2021-03-04 19:53:10 +0100

    Agregar archivos de proyecto.

---
+++ SafeEntranceApp/SafeEntranceApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using SafeEntranceApp.ViewModels;
+
+namespace SafeEntranceApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
