ce4ddda28f443490dd405584e9b4c04e35211a12
Raulpemol <UO263743@uniovi.es>
2021-03-05 16:43:44 +0100

    Creado servicio de acceso a bd local

---
+++ SafeEntranceApp/SafeEntranceApp/Models/Visit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace SafeEntranceApp.Models
+{
+    class Visit
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string PlaceID { get; set; }
+        public DateTime EnterDateTime { get; set; }
+        public DateTime ExitDateTime { get; set; }
+    }
+}
