63ef5838d89f34af756b07fdda07cbb39707fd74
Raulpemol <UO263743@uniovi.es>
2021-03-05 12:38:08 +0100

    Mejorada interfaz de la pantalla de escaneo

--- SafeEntranceApp/SafeEntranceApp/ViewModels/ScannerViewModel.cs
+++ SafeEntranceApp/SafeEntranceApp/ViewModels/ScannerViewModel.cs
@@ -2,14 +2,48 @@
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using ZXing;
+
 
 namespace SafeEntranceApp.ViewModels
 {
     public class ScannerViewModel : BaseViewModel
     {
+        private string _actionEnabled;
+        public string ActionEnabled
+        {
+            get => _actionEnabled;
+            set
+            {
+                // LoadUserState
+                // Update ActionEnabled
+                SetProperty(ref _actionEnabled, value);
+            }
+        }
+
+        private bool _scannerVisibility;
+        public bool ScannerVisibility
+        {
+            get => _scannerVisibility;
+            set
+            {
+                SetProperty(ref _scannerVisibility, value);
+            }
+        }
+
+        public ICommand ActivateScanCommand => new Command(() => ScannerVisibility = !ScannerVisibility);
+
         public ScannerViewModel()
         {
             Title = "SafeEntrance";
+            ActionEnabled = "Entra a un local";
+            ScannerVisibility = false;
         }
+
+        public void ProcessCode(Result result)
+        {
+            
+        }
+
     }
 }
--- SafeEntranceApp/SafeEntranceApp/Views/ScannerPage.xaml.cs
+++ SafeEntranceApp/SafeEntranceApp/Views/ScannerPage.xaml.cs
@@ -5,6 +5,7 @@ using Xamarin.Forms.Xaml;
 using ZXing;
 using ZXing.Mobile;
 using SafeEntranceApp.ViewModels;
+using System.Threading.Tasks;
 
 namespace SafeEntranceApp.Views
 {
@@ -18,15 +19,37 @@ namespace SafeEntranceApp.Views
 
             viewModel = new ScannerViewModel();
             BindingContext = viewModel;
-            AnimateScanner();
+            LoadCustomComponents();
+        }
+
+        private void LoadCustomComponents()
+        {
+            Frame activateScanFrame = FindByName("activateScanFrame") as Frame;
+            var tapGestureRecognizer = new TapGestureRecognizer();
+
+            tapGestureRecognizer.Tapped += async (s, e) =>
+            {
+                var fadeOutAnimationTask = activateScanFrame.FadeTo(0.3, 200);
+                await Task.WhenAll(fadeOutAnimationTask);
+                var fadeInAnimationTask = activateScanFrame.FadeTo(1, 200);
+                await Task.WhenAll(fadeInAnimationTask);
+            };
+            activateScanFrame.GestureRecognizers.Add(tapGestureRecognizer);
         }
 
         public void OnScanResult(Result result)
         {
             Device.BeginInvokeOnMainThread(async () =>
             {
-                await DisplayAlert("Scanned result", "Barcode", "OK");
+                Frame activateScanFrame = FindByName("activateScanFrame") as Frame;
+                activateScanFrame.BackgroundColor = (Color)App.Current.Resources["Accent"];
+                viewModel.ProcessCode(result);
             });
+        }
+
+        private void OnActivateScanTapped(object sender, EventArgs e)
+        {
+            AnimateScanner();
         }
 
         private async void AnimateScanner()
