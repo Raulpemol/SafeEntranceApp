a5ad716139a5f3b356f061b77a0acc68f80d7d9d
Raulpemol <UO263743@uniovi.es>
2021-03-28 21:02:34 +0200

    Rough design of the view

---
+++ SafeEntranceApp/SafeEntranceApp/ViewModels/CreateAlertViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SafeEntranceApp.ViewModels
+{
+    class CreateAlertViewModel : BaseViewModel
+    {
+        public CreateAlertViewModel()
+        {
+            Title = "SafeEntrance";
+        }
+    }
+}
---
+++ SafeEntranceApp/SafeEntranceApp/Views/CreateAlertPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using SafeEntranceApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SafeEntranceApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreateAlertPage : ContentPage
+    {
+        private CreateAlertViewModel viewModel;
+
+        public CreateAlertPage()
+        {
+            InitializeComponent();
+
+            viewModel = new CreateAlertViewModel();
+            BindingContext = viewModel;
+        }
+    }
+}
