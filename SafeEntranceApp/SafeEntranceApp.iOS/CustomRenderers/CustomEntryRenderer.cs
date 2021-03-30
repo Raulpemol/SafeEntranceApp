52f842116c565f7e9b479dd5318571d3f0495362
Raulpemol <UO263743@uniovi.es>
2021-03-30 13:58:31 +0200

    Changed entry line colors

---
+++ SafeEntranceApp/SafeEntranceApp.iOS/CustomRenderers/CustomEntryRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using SafeEntranceApp.iOS.CustomRenderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace SafeEntranceApp.iOS.CustomRenderers
+{
+    class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            Control.Layer.BorderColor = UIColor.FromRGB(0, 109, 91).CGColor;
+            Control.Layer.BorderWidth = 1;
+        }
+    }
+}
