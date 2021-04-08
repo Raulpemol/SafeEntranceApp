507ba6fa183bad59b723764b188666e93aa7e1e9
Raulpemol <UO263743@uniovi.es>
2021-04-08 18:11:58 +0200

    Added new tab and changed tab icons

--- SafeEntranceApp/SafeEntranceApp.Android/Resources/Resource.designer.cs
+++ SafeEntranceApp/SafeEntranceApp.Android/Resources/Resource.designer.cs
@@ -8385,19 +8385,25 @@ namespace SafeEntranceApp.Droid
 			public const int qr_code_scan = 2131165307;
 			
 			// aapt resource value: 0x7F07007C
-			public const int tab_about = 2131165308;
+			public const int shield_alert = 2131165308;
 			
 			// aapt resource value: 0x7F07007D
-			public const int tab_feed = 2131165309;
+			public const int tab_about = 2131165309;
 			
 			// aapt resource value: 0x7F07007E
-			public const int tooltip_frame_dark = 2131165310;
+			public const int tab_feed = 2131165310;
 			
 			// aapt resource value: 0x7F07007F
-			public const int tooltip_frame_light = 2131165311;
+			public const int tooltip_frame_dark = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int xamarin_logo = 2131165312;
+			public const int tooltip_frame_light = 2131165312;
+			
+			// aapt resource value: 0x7F070081
+			public const int virus = 2131165313;
+			
+			// aapt resource value: 0x7F070082
+			public const int xamarin_logo = 2131165314;
 			
 			static Drawable()
 			{
